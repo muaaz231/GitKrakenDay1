f8114e1748e8ddafbed2319a9812ab5479af60ea
muaaz231 <muaaz.saslam@gmail.com>
2025-11-18 13:56:35 -0500

    Basic thread count 1000 times to a million (one for each of the 1000 threads)

--- GitThread.java
+++ GitThread.java
@@ -2,18 +2,34 @@ import java.util.*;
 
 
 
-public class GitThread
+public class GitThread implements Runnable
 {
-	class MyThread extends Thread
+	private long count;
+
+	public GitThread()
 	{
 
 	}
 
-
+	@Override
+	public void run()
+	{
+		while(count < 10000000)
+		{
+			count++;
+		}
+		System.out.println(count);
+	}
 
 
 	public static void main(String[] args)
 	{
+		Thread[] threads = new Thread[1000];
 
+		for(int i = 0; i < threads.length; i++)
+		{
+			threads[i] = new Thread(new GitThread());
+			threads[i].start();
+		}
 	}
 }
